bfbc11a41a945f1d95946dec34dfef8db00200d9
CS884335 <CS884335@wcupa.edu>
2022-01-26 00:23:48 -0500

    changed url to hosted JSON server API

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 
   //Fetch Tasks
   const fetchTasks = async () => {
-    const res = await fetch('http://localhost:5000/tasks')
+    const res = await fetch('https://my-json-server.typicode.com/sadoffc/task-tracker-JSON-server')
     const data = await res.json()
 
     return data
@@ -29,7 +29,7 @@ const App = () => {
 
   //Fetch Task
   const fetchTask = async (id) => {
-    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const res = await fetch(`https://my-json-server.typicode.com/sadoffc/task-tracker-JSON-server/${id}`)
     const data = await res.json()
 
     return data
@@ -37,7 +37,7 @@ const App = () => {
 
   //Add Task
   const addTask = async (task) => {
-    const res = await fetch('http://localhost:5000/tasks', {
+    const res = await fetch('https://my-json-server.typicode.com/sadoffc/task-tracker-JSON-server', {
       method: 'POST',
       headers: {
         'Content-type': 'application/json',
@@ -52,7 +52,7 @@ const App = () => {
 
   //Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, {
+    await fetch(`https://my-json-server.typicode.com/sadoffc/task-tracker-JSON-server/${id}`, {
       method: 'DELETE',
     })
 
@@ -64,7 +64,7 @@ const App = () => {
     const taskToToggle = await fetchTask(id)
     const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
 
-    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+    const res = await fetch(`https://my-json-server.typicode.com/sadoffc/task-tracker-JSON-server/${id}`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json'
